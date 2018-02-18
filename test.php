77abf2d83093a93c410a4b8ddb1f96319bbeb503
Nix2k <mihail.bokov@gmail.com>
2018-02-18 18:31:02 +0300

    v1.0

--- test.php
+++ test.php
@@ -17,33 +17,42 @@
 				$username = htmlspecialchars(strip_tags($_GET['username']));
 				$scores = 0; //количество верных ответов
 				$numOfQuestions = 0; //количество вопросов
-				/*echo "<h1>Результаты прохождения теста: $title->title</h1>";
+				//echo "<h1>Результаты прохождения теста: $title->title</h1>";
 				foreach ($test as $qId => $question) {
-					echo '<p><b>'.htmlspecialchars(strip_tags($question->text)).'</b></p>';
+					//echo '<p><b>'.htmlspecialchars(strip_tags($question->text)).'</b></p>';
 					$isRight = true;
 					foreach ($question->options as $optionId => $option) {
 						if ((isset($_GET[$qId.'_'.$optionId])) xor ($option[1]==1))
 							$isRight = false;
 					}
 					if ($isRight) {
-						echo '<p style="color: green;">Верно</p>';
+						//echo '<p style="color: green;">Верно</p>';
 						$scores++;
 					}
-					else {
+					/*else {
 						echo '<p style="color: red;">Не верно</p>';
-					}
+					}*/
 					$numOfQuestions++;
 				}
 				$mark = round($scores*100/$numOfQuestions);
-				echo "<p><b>Количество набранных баллов $mark из 100</b></p>";*/
+				//echo "<p><b>Количество набранных баллов $mark из 100</b></p>";
 				//Генерация файла с сертификатом
 				$imgBlank = imagecreatefrompng(__DIR__.'/img/blank.png');
+				$xAx = round(imagesx($imgBlank) / 2);
 				$fontFile = __DIR__.'/fonts/MarckScript.ttf';
 				$textColor = imagecolorallocate($imgBlank, 0, 0, 0);
-				imagettftext($imgBlank,36,0,400,400,$textColor,$fontFile,"$username");
-				imagettftext($imgBlank,32,0,500,500,$textColor,$fontFile,"выполнил тест");
-				imagettftext($imgBlank,36,0,400,600,$textColor,$fontFile,"$title->title");
-				imagettftext($imgBlank,36,0,250,700,$textColor,$fontFile,"Результат тестирования: $mark из 100");
+				$textBox = imagettfbbox(36, 0, $fontFile, "$username");
+				$textHalfWidth = round(($textBox[2] - $textBox[0])/2);
+				imagettftext($imgBlank,36,0,$xAx-$textHalfWidth,350,$textColor,$fontFile,"$username");
+				$textBox = imagettfbbox(36, 0, $fontFile, "выполнил тест");
+				$textHalfWidth = round(($textBox[2] - $textBox[0])/2);
+				imagettftext($imgBlank,32,0,$xAx-$textHalfWidth,425,$textColor,$fontFile,"выполнил тест");
+				$textBox = imagettfbbox(36, 0, $fontFile, "$title->title");
+				$textHalfWidth = round(($textBox[2] - $textBox[0])/2);
+				imagettftext($imgBlank,36,0,$xAx-$textHalfWidth,500,$textColor,$fontFile,"$title->title");
+				$textBox = imagettfbbox(36, 0, $fontFile, "Результат тестирования: $mark из 100");
+				$textHalfWidth = round(($textBox[2] - $textBox[0])/2);
+				imagettftext($imgBlank,36,0,$xAx-$textHalfWidth,575,$textColor,$fontFile,"Результат тестирования: $mark из 100");
 				header('Content-Type: image/png');
 				header('Content-Disposition: attachment; filename="certificate.png"');
 				imagepng($imgBlank);
